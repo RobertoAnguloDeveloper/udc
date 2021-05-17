7ae7df8c9b32e18ac8e593d08f0e309466b88a1e
hacke <hacke@LAPTOP-CBQJFFHG>
2021-05-16 23:40:34 -0500

    AAU2-Ej1-LO FACIL

--- src/Ej1.java
+++ src/Ej1.java
@@ -18,17 +18,61 @@ public class Ej1 {
         int [][] cuadroMagico = new int[dimension][dimension];
         int numeroMedio = (dimension*dimension+1)/2;
         
-        int medioAdress = (dimension+1)/2;
+        int medioAdress = ((dimension+1)/2)-1;
         cuadroMagico[medioAdress][medioAdress] = numeroMedio;
 
 //        System.out.println("El numero del medio es: "+numeroMedio
 //                +"\nSu direccion es: ["+medioAdress+"]"+"["+medioAdress+"]");
 
         int [] numeros = new int[dimension*dimension];
-
-        for(int i = 0; i<numeros.length; i++){
+        int [] pares = new int[numeros.length/2];
+        int [] esquina = new int[4];
+                
+        for(int i = 0; i < numeros.length; i++){
             numeros[i] = i+1;
         }
+        
+        int contadorEsquina = 0;
+        for(int i = 0; i < numeros.length; i++){
+            if(numeros[i] % 2 == 0){
+                if(contadorEsquina < esquina.length){
+                    esquina[contadorEsquina] = numeros[i];
+                    contadorEsquina++;
+                }
+            }
+        }
+        
+        //INSERTANDO ESQUINAS
+        for(int i = 0; i < cuadroMagico.length; i++){
+            for(int j = 0; j < cuadroMagico[0].length; j++){
+                if(i == 0 && j == 0){
+                    cuadroMagico[i][j] = esquina[0];
+                }else if(i == 0 && j == cuadroMagico.length-1){
+                    cuadroMagico[i][j] = esquina[1];
+                }else if(i == cuadroMagico.length-1 && j == 0){
+                    cuadroMagico[i][j] = esquina[2];
+                }else if(i == cuadroMagico.length-1 && j == cuadroMagico.length-1){
+                    cuadroMagico[i][j] = esquina[3];
+                }
+            }
+        }
+        
+        //IMPRIME MATRIZ FINAL
+        for(int i = 0; i < cuadroMagico.length; i++){
+            for(int j = 0; j < cuadroMagico[0].length; j++){
+                System.out.print("\t"+cuadroMagico[i][j]);
+                if(j == cuadroMagico[0].length-1){
+                    System.out.println("");
+                }
+            }
+        }
+        
+//        for (int numero : numeros) {
+//            System.out.print("\t"+numero);
+//        }
+//        System.out.println("");
+
+        
     }
     
 }
