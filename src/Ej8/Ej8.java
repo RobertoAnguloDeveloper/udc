89708c26550b6e05b68a1e5770875a6346e10342
hacke <hacke@LAPTOP-CBQJFFHG>
2021-06-14 16:34:38 -0500

    AAU3V3 - EJ8 - CORREGIDO 4

--- src/Ej8/Ej8.java
+++ src/Ej8/Ej8.java
@@ -21,22 +21,37 @@ public class Ej8 {
         Nodo nodoC = new Nodo("C");
         Nodo nodoD = new Nodo("D");
         Nodo nodoE = new Nodo("E");
-        
+        Nodo nodoNull = new Nodo(null);
+        Nodo nodoNull2 = new Nodo(null);
+        Nodo nodoNull3 = new Nodo(null);
+        Nodo nodoNull4 = new Nodo(null);
+        Nodo nodoNull5 = new Nodo(null);
         ColaCircular c = new ColaCircular(6);
         
         c.encolarDerecha(nodoA);
         c.encolarDerecha(nodoB);
         c.encolarDerecha(nodoC);
+        c.encolarDerecha(nodoNull);
+        c.encolarDerecha(nodoNull2);
+        c.encolarDerecha(nodoNull3);
+        System.out.println("1. Insertar por el extremo derecho tres elementos: A, B y C.");
         c.imprimir();
         
         c.desencolar();
+        c.encolar(nodoNull4);
+        System.out.println("\n2. Eliminar por el extremo izquierdo un elemento.");
         c.imprimir();
         
+        c.desencolar();
         c.encolar(nodoD);
         c.encolar(nodoE);
+        c.desencolarDerecha();
+        System.out.println("\n3. Insertar por el extremo izquierdo dos elementos: D y E.");
         c.imprimir();
         
         c.desencolarDerecha();
+        c.encolarDerecha(nodoNull5);
+        System.out.println("\n4. Eliminar por la derecha un elemento.");
         c.imprimir();
         
     }
