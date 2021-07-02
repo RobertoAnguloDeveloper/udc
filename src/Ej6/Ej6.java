d6a948aa1322d7ea0d50bcb2cf0cbac375860a11
RANGULOT <rangulot@unicartagena.edu.co>
2021-07-01 23:06:32 -0500

    Ej6 - LLENANDO MATRIZ DE ADYACENCIA

--- src/Ej6/Ej6.java
+++ src/Ej6/Ej6.java
@@ -21,15 +21,30 @@ public class Ej6{
         int [][] matrizAdyacencia = new int[cantidadVertices][cantidadVertices];
 
         
-        System.out.println("ESTABLEZCA LA RELACION ENTRE LOS VERTICES");
+        System.out.println("\nESTABLEZCA LA RELACION ENTRE LOS VERTICES");
         System.out.println("*****************************************");
         for (int i = 0; i < matrizAdyacencia.length; i++) {
             for (int j = 0; j < matrizAdyacencia[0].length; j++) {
                 System.out.print("\t"+vertices.get(i)+" y "+vertices.get(j)+"\n[1 para SI/ 0 para NO]: ");
                 int relacion = key.nextInt();
-
+                System.out.println();
                 matrizAdyacencia[i][j] = relacion;
             }
+        }
+
+        //IMPRIME MATRIZ DE ADYACENCIA
+        System.out.println("\nMATRIZ DE ADYACENCIA");
+        System.out.println("********************");
+        for (int k = 0;  k < vertices.size(); k++) {
+            System.out.print("  "+vertices.get(k)+"    ");
+        }
+        System.out.println("\n-----------------");
+        for (int i = 0; i < matrizAdyacencia.length; i++) {
+            System.out.print(vertices.get(i)+"|");
+            for (int j = 0; j < matrizAdyacencia[0].length; j++) {
+                System.out.print(matrizAdyacencia[i][j]+"\t");
+            }
+            System.out.println();
         }
     }
 }
