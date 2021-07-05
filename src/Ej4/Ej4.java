4967b0d19dccaa3cda4f6c851ee21090d1b8a8f6
RANGULOT <rangulot@unicartagena.edu.co>
2021-07-05 00:02:17 -0500

    Ej4 - EN PROCESO

--- src/Ej4/Ej4.java
+++ src/Ej4/Ej4.java
@@ -1,9 +1,15 @@
 package Ej4;
 
 import EstructuraDatos.ArbolB;
+import EstructuraDatos.Nodo;
 
 public class Ej4 {
     public static void main(String[] args) {
+        Nodo nodo1 = new Nodo(1);
         ArbolB arbolB = new ArbolB(5);
+
+        arbolB.insertar(nodo1);
+
+        System.out.println("PAGINARAIZ[0] = "+arbolB.getPaginaRaiz()[0].getDato());
     }
 }
--- src/EstructuraDatos/ArbolB.java
+++ src/EstructuraDatos/ArbolB.java
@@ -2,16 +2,20 @@ package EstructuraDatos;
 
 public class ArbolB {
     private int grado;
-    private NodoB raiz;
+    private Nodo [] paginaRaiz;
 
     public ArbolB(int grado) {
         this.grado = grado;
-        raiz = new NodoB(grado);
+        paginaRaiz = new Nodo[grado-1];
     }
 
-    public void insertar(int valor) {
-        if(raiz.getClaves()[0] == 0){
-            raiz
+    public void insertar(Nodo nodo) {
+        if(paginaRaiz[0] == null){
+            paginaRaiz[0] = nodo;
         }
+    }
+
+    public Nodo [] getPaginaRaiz() {
+        return paginaRaiz;
     }
 }
